fbbb266ef3428089cefa41a81fbc4d756550bc5e
Chu <averysbchu@gmail.com>
2025-11-15 11:09:06 +0800

    added more delete locations

--- app/DBApp/models/location.js
+++ app/DBApp/models/location.js
@@ -3,7 +3,6 @@ const knex = require('./knexfile')
 // INSERT
 exports.insertLocation = (loc) => knex('Orders').insert(loc)
 
-
 // VIEW
 exports.viewAllLocations = () => knex('Location').select('*')
 exports.getLocationsByContinent = (c) => knex('Location').where('continent',c)
@@ -14,3 +13,8 @@ exports.getLocationsByStreet = (c) => knex('Location').where('street',c)
 
 // DELETE
 exports.deleteLocation = (id) => knex('Location').where('location_id',id).delete()
+exports.deleteLocationsByContinent = (c) => knex('Location').where('continent',c).delete()
+exports.deleteLocationsByCountry = (c, co) => knex('Location').where('country',c).where('continent',co).delete()
+exports.deleteLocationsByState = (c, s) => knex('Location').where('country',c).where('state',s).delete()
+exports.deleteLocationsByCity = (c, s, co) => knex('Location').where('city',c).where('country',co).where('state',s).delete()
+exports.deleteLocationsByStreet = (c,s,ci,st) => knex('Location').where('street',st).where('city',ci).where('country',c).where('state',s).delete()
