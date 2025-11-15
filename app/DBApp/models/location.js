cc86558cc63090b16ce74ec8658cf556ef34a28c
Chu <averysbchu@gmail.com>
2025-11-15 10:55:34 +0800

    added insert & delete

--- app/DBApp/models/location.js
+++ app/DBApp/models/location.js
@@ -1,5 +1,9 @@
 const knex = require('./knexfile')
 
+// INSERT
+exports.insertLocation = (loc) => knex('Orders').insert(loc)
+
+
 // VIEW
 exports.viewAllLocations = () => knex('Location').select('*')
 exports.getLocationsByContinent = (c) => knex('Location').where('continent',c)
@@ -7,3 +11,6 @@ exports.getLocationsByCountry = (c) => knex('Location').where('country',c)
 exports.getLocationsByState = (c) => knex('Location').where('state',c)
 exports.getLocationsByCity = (c) => knex('Location').where('city',c)
 exports.getLocationsByStreet = (c) => knex('Location').where('street',c)
+
+// DELETE
+exports.deleteLocation = (id) => knex('Location').where('location_id',id).delete()
